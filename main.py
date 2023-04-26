74b9b404156387f554c8981f9e82d6f5f8d80592
Mac0r0ni_And_Weed <61136278+Mac0r0ni@users.noreply.github.com>
2023-04-26 05:45:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if battery_level < 51:
     plugged_in = "NOT PLUGGED IN"
 else:
     plugged_in = "PLUGGED IN"
-self.client.send_chat_message(group_jid, f"Battery level: {battery_level}% ({plugged_in})")
+self.client.send_chat_message(group_jid, f"Battery level: {battery_level}% | ({plugged_in})")
