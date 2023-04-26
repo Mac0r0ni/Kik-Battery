ad4e278ec067d06c6506f635eaad1ab80ce2cf46
Mac0r0ni_And_Weed <61136278+Mac0r0ni@users.noreply.github.com>
2023-04-26 05:44:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import random
 
 battery_level = rand.int(0, 100)
-self.client.send_chat_message(group_jid, f"Battery level: {battery_level}% (Plugged In)")
+if battery_level < 51:
+    plugged_in = "NOT PLUGGED IN"
+else:
+    plugged_in = "PLUGGED IN"
+self.client.send_chat_message(group_jid, f"Battery level: {battery_level}% ({plugged_in})")
